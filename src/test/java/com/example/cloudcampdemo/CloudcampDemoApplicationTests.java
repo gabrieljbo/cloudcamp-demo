8cea590aca9c548a8d07cb27d87095be9b9e6590
Gabriel Buitrago <gabrieljbo@gmail.com>
2024-02-01 19:08:43 -0500

    Initial commit

---
+++ src/test/java/com/example/cloudcampdemo/CloudcampDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudcampdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudcampDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
